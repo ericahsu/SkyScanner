29d50b498bc32ac989e2dc1c21cff74135f44273
Erica Hsu <eyhsu@andrew.cmu.edu>
2021-03-19 22:01:52 -0700

    Update settings.py

--- skyscanner/skyscanner/settings.py
+++ skyscanner/skyscanner/settings.py
@@ -15,15 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p_p&zpif(u1$m7phmzz%#+vw_ijn^uwu7b@a@i@tz@32x7%+-j'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
